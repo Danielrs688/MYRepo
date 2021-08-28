63dbd44945ce503a3d27e31336b15874e2a9a8aa
Daniel <daniel_rs_remo@hotmail.com>
2021-08-28 09:38:47 +0200

    practica 3.1

---
+++ Modulo 3.1/app.js
@@ -0,0 +1,6 @@
+const booking1 = {count : 3, price: 100};
+const booking2 = {count : 5, price: 100};
+
+// Mostrar la suma del número de noches de las dos reservas
+
+console.log(booking1.count + booking2.count );
