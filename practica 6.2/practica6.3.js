8b499cb0ab24c688f14ffb1adf91e0c5c570cea0
Daniel <daniel_rs_remo@hotmail.com>
2021-10-18 12:26:09 +0200

    practica 6.3

---
+++ practica 6.2/practica6.3.js
@@ -0,0 +1,33 @@
+var plainAlphabet = "abcdefghijklmnopqrstuvwxyz:()!¡,'";
+var encryptedAlphabet = "qw,ert(yuio'pa:sdfg!hjklz¡xcv)bnm";
+
+var text = "y:'q phae:";
+
+var searchIndex = (alphabet, letter) => {
+    for (var i = 0; i < alphabet.length; i++) {
+        if (alphabet[i] === letter) return i;
+    }
+    return -1;
+}
+
+var transformLetter = (letter, origin, destino) => {
+    var letterIndex = searchIndex (origin, letter);
+        if (letterIndex === -1) {
+            return letter;
+        } 
+        else {
+            return destino[letterIndex];
+        }
+    }
+
+    var transformMessage = message => {
+        var messageLowerCase = message.toLowerCase ();
+        var result = "";
+
+        for (var letter of messageLowerCase) {
+            result = result + transformLetter (letter, encryptedAlphabet, plainAlphabet);
+        }
+        return result;
+    }
+
+    console.log(transformMessage(text));
