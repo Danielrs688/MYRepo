076c28941ea35698cfc0c2c403051ea055f066ca
Daniel <daniel_rs_remo@hotmail.com>
2022-10-17 20:01:37 +0200

    practica7

--- Modulo 7/opcional7.1.js
+++ Modulo 7/opcional7.1.js
@@ -1,8 +1,12 @@
 //* Validar sintaxis tarjeta de crédito
+//* Caso 1  Vamos a validar los siguiente formatos (todos válidos) 5299 6400 0000 0000  5299-6400-0000-0000   5299640000000000 
+
+// const pattern = /^5[1-5]\d{2}((\s?|\-)\d{4}){3}$/ 
+// const values = ['5299 6400 0000 0000', '5299-6400-0000-0000', '5299640000000000'];
 
 const pattern = /^50|51|52|53|54|55$/;
 const values = ['52', '53', '54', '55', '50', '51'];
 
 values.forEach(value => {
-    console.log('Regex matchs with'+ value +'? ->', pattern.exec(value));
-})
+    console.log(value + " =  ", pattern.test(value));
+});
